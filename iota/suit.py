f107826ec5f16897b63554743e618d0c09e1c03a
Cleber Peter <clebes.peter@gmail.com>
2020-08-02 14:54:11 -0300

    updated directories

--- iota/suit.py
+++
@@ -1,6 +0,0 @@
-class fotaSuit:
-    def __init__(self, hostBroker, portBroker):
-        self.hostBroker = hostBroker
-        self.portBroker = portBroker
-        print("fotaSuit initialized.")
-        
