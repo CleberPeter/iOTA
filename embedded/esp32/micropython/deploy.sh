efb01857aba3aefcbf181e9ce962f3c6940bade5
Cleber Peter <clebes.peter@gmail.com>
2020-08-04 19:44:04 -0300

    fixed the script to collect the files from the relative directory

--- embedded/esp32/micropython/deploy.sh
+++ embedded/esp32/micropython/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # script to deploy esp32
 
-ampy --port /dev/ttyUSB0 --baud 115200 put main.py
-ampy --port /dev/ttyUSB0 --baud 115200 put suit.py
+BASEDIR=$(dirname "$0")
+
+ampy --port /dev/ttyUSB0 --baud 115200 put "$BASEDIR"/main.py
+ampy --port /dev/ttyUSB0 --baud 115200 put "$BASEDIR"/suit.py
 picocom /dev/ttyUSB0 -b115200
