0052bc30ed5eee975f405d2ab4cd8d1a91767335
Cleber Peter <clebes.peter@gmail.com>
2020-08-09 16:46:17 -0300

    create manifest class

---
+++ deploymentTools/python/manifest.py
@@ -0,0 +1,5 @@
+class _manifest:
+    def __init__(self, type, dateExpiration, incrementalNumber):
+        self.type = type
+        self.dateExpiration = dateExpiration
+        self.incrementalNumber = incrementalNumber
